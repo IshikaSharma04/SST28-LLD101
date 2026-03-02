01515a154b8dc554943f094afa88c4616e8fda6c
IshikaSharma04 <ishika.24bcs10352@sst.scaler.com>
2026-03-02 17:12:02 +0530

    Update IncidentTicket.java

--- immutable-tickets/src/com/example/tickets/IncidentTicket.java
+++ immutable-tickets/src/com/example/tickets/IncidentTicket.java
@@ -2,6 +2,7 @@ package com.example.tickets;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 /**
  * INTENTION: A ticket should be an immutable record-like object.
